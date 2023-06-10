9791ec407ea5ce99fa34689676e894f40349aa5f
ahmed <ahmed-abotaleb@outlook.com>
2023-06-10 09:19:16 -0400

    first prototype

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,15 @@ const uploads = multer({dest: `./assets/temp`})
 const app = createApplication();
 
 
-app.post("/ahmedmohsen", uploads.array('audio', 3),async (req: Request, res: Response) => {
-    try{
-        const file = await ahmedMohsenHandler(req.files as  Express.Multer.File[]);
-        res.send('done')
-    }catch (e){
-        console.error(e)
+app.post("/ahmedmohsen", uploads.array('audio', 3), async (req: Request, res: Response) => {
+    try {
+        const file = await ahmedMohsenHandler(req.files as Express.Multer.File[]);
+        res.sendFile(file)
+    } catch (e: unknown) {
+        if (e instanceof Error)
+            console.error(e, e.stack)
         res.status(500)
-        res.send("something wrong")
+        res.send(`${e}`)
     }
 
 })
--- src/files-sevice.ts
+++ src/files-sevice.ts
@@ -1,45 +1,97 @@
 import ffmpeg from "fluent-ffmpeg";
 import * as fs from "fs/promises";
 import path from "path";
+import crypto from 'crypto'
+import {exec} from "child_process";
 
 
 export async function ahmedMohsenHandler(files: Express.Multer.File[]) {
-    const filePaths = files.map(x => x.path);
-    try {
-        return await concatenateAudioFiles(filePaths);
-    } catch (e) {
-        throw (e);
-    } finally {
-        deleteFiles(filePaths);
-    }
+    const filePaths = files.map(x => path.resolve(x.path));
+    return concatenateAudioFiles(filePaths);
 }
 
 
-const concatenateAudioFiles = (files: string[]) => {
-    let outputFilePath = path.join('./assets/concatenated', `${new Date()}.mp3`);
-    const promise: Promise<string> = new Promise((resolve, reject) => {
-        // let command = ffmpeg();
+const concatenateAudioFiles = async (userFiles: string[]) => {
+    const outputFilePath = path.resolve(path.join('./assets/concatenated', `${crypto.randomUUID()}-${new Date().toISOString()}.mp3`));
 
-        ffmpeg()
-            .input(files[0])
-            .input(files[1])
-            .input(files[2])
-            .on("error", (err) => {
-                console.error("An error occurred: " + err.message);
-                reject(err)
-            })
-            .on("end", (output: string) => {
-                console.log("Audio files concatenated successfully!");
-                resolve(path.resolve(output))
-            })
-            .concat(outputFilePath)
-            .run();
+    let ahmedMohsenFiles = await fs.readdir(path.resolve('./assets/original-ahmed-mohsen'))
+    ahmedMohsenFiles = ahmedMohsenFiles.map((n) => path.resolve(path.join('./assets/original-ahmed-mohsen', n)))
+
+    console.log(ahmedMohsenFiles)
+
+    let audioFiles: string[];
+    if (userFiles.length === 3) {
+        audioFiles = [
+            userFiles[0],
+            ahmedMohsenFiles[0],
+            userFiles[1],
+            ahmedMohsenFiles[1],
+            userFiles[2],
+            ahmedMohsenFiles[2]
+        ]
+    } else {
+        audioFiles = [
+            ahmedMohsenFiles[0],
+            userFiles[1],
+            ahmedMohsenFiles[1],
+            userFiles[2],
+            ahmedMohsenFiles[2]
+        ]
+    }
+
+    return  new Promise<string>(async (resolve, reject) => {
+        const command = ffmpeg();
+
+        try {
+            for (let index = 0; index < audioFiles.length; index++) {
+                const streamTypes = await getStreamTypes(audioFiles[index]);
+                const inputIndex = streamTypes.indexOf('audio');
+                if (inputIndex >= 0) {
+                    command.input(audioFiles[index]);
+                } else {
+                    console.error(`No audio stream found in file: ${audioFiles[index]}`);
+                }
+            }
+
+
+            let filterComplex = audioFiles.map((input, index) => `[${index}:a]`).join('');
+            filterComplex += `concat=n=${audioFiles.length}:v=0:a=1[outa]`;
+
+            command.complexFilter(filterComplex, 'outa');
+            command.output(outputFilePath);
+
+            command.on('end', () => {
+                console.log('Audio files concatenated successfully');
+                deleteFiles(userFiles)
+                resolve(outputFilePath);
+            }).on('error', (err) => {
+                console.error('Error concatenating audio files:', err);
+                reject(err);
+            }).run();
+
+        } catch (error) {
+            console.error('Error retrieving stream information:', error);
+            throw error
+        }
     });
-    return promise;
 };
 
 function deleteFiles(files: string[]) {
     for (let file of files) {
-        fs.unlink(path.absolute())
+        fs.unlink(file)
     }
+}
+
+function getStreamTypes(file: string) {
+    return new Promise<string[]>((resolve, reject) => {
+        exec(`ffprobe -v error -show_entries stream=codec_type -of csv=p=0 ${file}`, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            const streamTypes = stdout.trim().split('\n');
+            resolve(streamTypes);
+        });
+    });
 }
